512bc88cf0ca33db22383d459cd3a15fd782459a
Yenuka Ranathunga <yenukaadarsha93@gmail.com>
2025-06-30 16:01:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined? "5ytnCRwI#7slwI1sG8-h6zO4d3BfdDZgDfM3afmS-EJzJruBYxd8":process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined? "tm9DjS4Y#u2zLMGsxiLXLd6s-oZN2aGfXykg1jL0_XYKh1Hsl2Q0":process.env.SESSION_ID,
 MONGODB: process.env.MONGODB || "mongodb://mongo:quCNimqLfAodskIgVRRlZzAaJMDNSPIF@shinkansen.proxy.rlwy.net:25638",
 GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyDzyQ7tnQkquWX8bc1y9TE0auCHbC_QQO8",
 tokenm: process.env.GITHUB_TOKEN || "false",
